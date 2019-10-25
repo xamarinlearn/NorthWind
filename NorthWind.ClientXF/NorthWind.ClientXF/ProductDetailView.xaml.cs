1ea756476e722193d054badfc5497b7eed34ecf0
jese.alvarado@sv.cds <jese.alvarado@sv.cds>
2019-10-25 05:26:41 -0600

    Form detail

---
+++ NorthWind.ClientXF/NorthWind.ClientXF/ProductDetailView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NorthWind.ClientXF
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProductDetailView : ContentPage
+	{
+		public ProductDetailView ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnUpdate_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
